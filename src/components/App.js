6edcb66b4726a7c9a23805dddd4761c8d96c1c8e
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-04-23 16:04:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,63 +1,18 @@
 import React from "react";
-import "./../styles/App.css";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ItemList from './ItemList.js';
+import ItemDetail from './ItemDetail.js';
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        {/* Do not remove the main div */}
+    <div>
+      <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <h1>Item List</h1>
-                <ul>
-                  <li>
-                    <Link to={"/items/1"}>Item 1</Link>{" "}
-                  </li>
-                  <li>
-                    <Link to={"/items/2"}>Item 2</Link>{" "}
-                  </li>
-                  <li>
-                    <Link to={"/items/3"}>Item 3</Link>{" "}
-                  </li>
-                </ul>
-              </div>
-            }
-          />
-          <Route
-            path="/items/1"
-            element={
-              <div>
-                <h1>Item 1</h1>
-                <p>Description for Item 1</p>
-              </div>
-            }
-          />
-          <Route
-            path="/items/2"
-            element={
-              <div>
-                <h1>Item 2</h1>
-                <p>Description for Item 2</p>
-              </div>
-            }
-          />
-          <Route
-            path="/items/3"
-            element={
-              <div>
-                <h1>Item 3</h1>
-                <p>Description for Item 3</p>
-              </div>
-            }
-          />
+          <Route path="/" element={<ItemList />} />
+          <Route path="/item/:itemId" element={<ItemDetail />} />
         </Routes>
-      </div>
-    </BrowserRouter>
-  );
-};
-
-export default App;
+      </BrowserRouter>
+      {/* Do not remove the main div */}
+    </div>
+  )
+}
+export default App
