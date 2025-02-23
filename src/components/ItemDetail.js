e8e113a084747eeff14c81678596081354becad6
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-02-24 01:08:53 +0530

    Update ItemDetail.js

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-const ItemDetail = () => {
-    let {id}=useParams();
-    // let
+
+const ItemDetail = ({items}) => {
+    const {id} = useParams();
+    const item = items.find(item => item.id === parseInt(id))
   return (
     <div>
-
-         <h1>Item {id}</h1>
-      <p>Description for Item {id}</p>
+        <h1>Item {item.id}</h1>
+        <p>{item.description}</p>
     </div>
   )
 }
